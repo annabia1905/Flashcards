adf540867805efc8b8e982e1ed5a9d654174de51
fenah <ana.ribas31@escola.pr.gov.br>
2025-07-01 17:11:54 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criarCartao(
+    'CIÊNCIAS'
+    'como é feito a ftossinteses?'
+    'pelo sol'
+)
+
+criarCartao(
+    'programação'
+    'O que é um JavaScript?'
+    'é uma linguagem de programação'
+)
+
+criarCartao(
+    'geografia'
+    'Qual é a capital do brasil'
+    'Brasilía'
+    console.log( categoria, pergunta, resposta)
+    
+)
+}
